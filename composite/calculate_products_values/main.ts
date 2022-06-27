150cbf17d68e868b3f8acccdd55ec6f1f7483de1
Kaio Viana <kaiu.viana@hotmail.com>
2022-06-27 09:08:03 -0300

    feat: init products values example setup

--- composite/calculate_products_values/main.ts
+++ composite/calculate_products_values/main.ts
@@ -1,6 +1,4 @@
 /**
- * This code exemplifies an application which calculate total value of all products in an box.
- * The box could have another boxes with product into.
- * The composite patterns is used to solve this problem.
+ * This code exemplifies an application which calculate total value of all orders of an client
+ * requested in marketplace applying the composite pattern
  */
-
