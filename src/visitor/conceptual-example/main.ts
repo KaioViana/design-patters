7d3d9672524c0b2185a9d6c575322e4c7f57f944
kaioViana <kaiu.viana@hotmail.com>
2023-03-21 08:53:21 -0300

    feat: visitor pattern

---
+++ src/visitor/conceptual-example/main.ts
@@ -0,0 +1,69 @@
+interface Component {
+  accept(visitor: Visitor): void;
+}
+
+interface Visitor {
+  visitConcreteComponentA(element: ConcreteComponentA): void;
+  visitConcreteComponentB(element: ConcreteComponentB): void;
+}
+
+class ConcreteComponentA implements Component {
+  public accept(visitor: Visitor): void {
+    visitor.visitConcreteComponentA(this);
+  }
+
+  public exclusiveMethodOfConcreteComponentA(): string {
+    return 'A';
+  }
+}
+
+class ConcreteComponentB implements Component {
+  public accept(visitor: Visitor): void {
+    visitor.visitConcreteComponentB(this);
+  }
+
+  public speciaMethodOfConcreteComponentB(): string {
+    return 'B'
+  }
+}
+
+class ConcreteVisitor1 implements Visitor {
+  public visitConcreteComponentA(element: ConcreteComponentA): void {
+    console.log(`${element.exclusiveMethodOfConcreteComponentA()} + ConcreteVisitor1`)
+  }
+
+  public visitConcreteComponentB(element: ConcreteComponentB): void {
+    console.log(`${element.speciaMethodOfConcreteComponentB()} + ConcreteVisitor1`);
+  }
+}
+
+class ConcreteVisitor2 implements Visitor {
+  public visitConcreteComponentA(element: ConcreteComponentA): void {
+    console.log(`${element.exclusiveMethodOfConcreteComponentA()} + ConcreteVisitor2`)
+  }
+
+  public visitConcreteComponentB(element: ConcreteComponentB): void {
+    console.log(`${element.speciaMethodOfConcreteComponentB()} + ConcreteVisitor2`);
+  }
+}
+
+function clienteCode(components: Component[], visitor: Visitor) {
+  for (const component of components) {
+    component.accept(visitor);
+  }
+}
+
+(() => {
+  const components = [
+    new ConcreteComponentA(),
+    new ConcreteComponentB(),
+  ];
+
+  const visitor1 = new ConcreteVisitor1();
+  clienteCode(components, visitor1);
+
+  console.log('');
+
+  const visitor2 = new ConcreteVisitor2();
+  clienteCode(components, visitor2);
+})()
